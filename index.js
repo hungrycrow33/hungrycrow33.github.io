0780ba62561f7e712592de3d9f32f4abbbf783ac
Hungry J <33iamj33@gmail.com>
2022-12-29 19:06:22 -0500

    templated created

---
+++ index.js
@@ -0,0 +1,16 @@
+function openTab(item,element,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(item).style.display = "block";
+    element.style.backgroundColor = color;
+  
+  }
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
