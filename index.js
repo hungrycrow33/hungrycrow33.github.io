c4a99a9050527bd6e19d1970fe3093eae51d032e
Hungry J <33iamj33@gmail.com>
2023-01-07 04:26:03 -0500

    updated jumping Game, not much changed portfolio

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function openTab(item,element,color) {
     var i, tabcontent, tablinks;
+    // erase all the contents before (so the selected one can be shown)
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
+      console.log("i: ",i)
       tabcontent[i].style.display = "none";
     }
+    // erase all the other buttons' color
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
     }
+    //background color of the button
     document.getElementById(item).style.display = "block";
     element.style.backgroundColor = color;
   
