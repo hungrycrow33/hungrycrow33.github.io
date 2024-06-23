86a5d7fda882b4ef0e9ea929a2199498c629a6ff
hungrycrow33 <j.jiyeonjung33@gmail.com>
2024-06-23 15:30:37 -0400

    structure set up for portfolio v2

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add any JavaScript functionality here
+// For example, you might want to add smooth scrolling or other interactive features
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Portfolio site loaded');
+});
